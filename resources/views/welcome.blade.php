e589bfef14c2a6e912b49307157d0a0a76c82d71
Ernest Walzel <ewalzel@gmail.com>
2022-10-21 11:57:10 +0200

    Test logging

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,7 @@
 </head>
 
 <body class="antialiased">
+    {{ dd('testing logging') }}
     <div
         class="flex relative items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
         @if (Route::has('login'))
