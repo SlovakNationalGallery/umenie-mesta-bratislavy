bd6716e2ca758b5d38bcb10e3faaebc0124e4c36
Ernest Walzel <ewalzel@gmail.com>
2022-10-21 13:14:15 +0200

    revert logging test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,6 @@
 </head>
 
 <body class="antialiased">
-    {{ Log::critical('testing logging') }}
     <div
         class="flex relative items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
         @if (Route::has('login'))
