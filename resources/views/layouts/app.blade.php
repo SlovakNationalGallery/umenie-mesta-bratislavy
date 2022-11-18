46c616d37025f7469ea50385b526489f276a69ea
Ernest Walzel <ewalzel@gmail.com>
2022-11-18 11:49:00 +0100

    Add nav to layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,37 @@
 </head>
 
 <body class="antialiased text-neutral-800">
+    <div class="p-4 bg-neutral-100">
+        <div class="p-4 border rounded-lg border-neutral-800 flex flex-col">
+            <h1 class="text-2xl text-center">
+                <span class="font-medium">Umenie</span>
+                mesta
+                <span class="font-medium">Bratislavy</span>
+            </h1>
+            <nav>
+                <ul class="text-lg flex justify-around mt-4 font-medium">
+                    <li>
+                        <a href="TODO">O projekte</a>
+                    </li>
+                    <li>
+                        <a href="TODO">Mapa a katalóg diel</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="mt-4 relative">
+            <img src="https://placeholder.pics/svg/300/DEDEDE/555555/mapa" class="w-full">
+            <div class="absolute inset-0 flex justify-center items-center">
+                <a href="TODO"
+                    class="text-lg bg-neutral-800 text-white inline-flex items-center rounded-full px-4 py-2">
+                    Objavte diela v okolí
+                    {{-- TODO get right icon --}}
+                    <x-icons.map-pin class="ml-1" />
+                </a>
+            </div>
+        </div>
+    </div>
 
     @yield('content')
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,37 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="p-4 bg-neutral-100">
-        <div class="p-4 border rounded-lg border-neutral-800 flex flex-col">
-            <h1 class="text-2xl text-center">
-                <span class="font-medium">Umenie</span>
-                mesta
-                <span class="font-medium">Bratislavy</span>
-            </h1>
-            <nav>
-                <ul class="text-lg flex justify-around mt-4 font-medium">
-                    <li>
-                        <a href="TODO">O projekte</a>
-                    </li>
-                    <li>
-                        <a href="TODO">Mapa a katalóg diel</a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-
-        <div class="mt-4 relative">
-            <img src="https://placeholder.pics/svg/300/DEDEDE/555555/mapa" class="w-full">
-            <div class="absolute inset-0 flex justify-center items-center">
-                <a href="TODO" class="text-lg bg-neutral-800 text-white inline-flex items-center rounded-full px-4 py-2">
-                    Objavte diela v okolí
-                    {{-- TODO get right icon --}}
-                    <x-icons.map-pin class="ml-1" />
-                </a>
-            </div>
-        </div>
-    </div>
-
     <section class="px-4">
 
         <h2 class="mt-10 text-3xl font-medium">
@@ -57,3 +26,4 @@
         </div>
     </section>
 @endsection
+
