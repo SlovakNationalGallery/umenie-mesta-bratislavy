f5980887c35119357f699a144fc178c6d9896e3d
Ernest Walzel <ewalzel@gmail.com>
2022-12-08 12:33:07 +0100

    Location district attribute

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +10,16 @@ class Location extends Model
 {
     use HasFactory;
     use \Staudenmeir\EloquentEagerLimit\HasEagerLimit;
+
+    protected function district(): Attribute
+    {
+        return Attribute::make(
+            get: function () {
+                if ($this->borough === 'Lamač') {
+                    return 'Bratislava IV';
+                }
+                // TODO
+            }
+        );
+    }
 }
