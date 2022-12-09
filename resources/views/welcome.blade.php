2f409d9a44786ad4cb20fe02b79321d62962962c
Ernest Walzel <ewalzel@gmail.com>
2022-12-09 12:53:50 +0100

    Include stats in front page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,25 @@
 
 @section('content')
     <div class="bg-neutral-100">
-        <div class="max-w-screen-3xl px-4 md:px-14 mx-auto md:flex">
+        <div class="max-w-screen-3xl px-4 md:px-14 mx-auto md:grid grid-cols-2">
             <div class="relative">
-                <home-map :locations="{{ $locations }}"></home-map>
+                <home-map :locations="{{ $locations }}" class="mx-auto"></home-map>
+            </div>
+
+            {{-- TODO format properly --}}
+            <div class="flex flex-col gap-y-2">
+                <div class="flex items-start gap-x-2">
+                    <span class="text-4xl font-medium">{{ $stats['artworks'] }}</span>
+                    <span class="leading-none mt-[0.15rem] text-base">diel<br /> v katalógu</span>
+                </div>
+                <div class="flex items-start gap-x-2">
+                    <span class="text-4xl font-medium">{{ $stats['boroughs'] }}/17</span>
+                    <span class="leading-none mt-[0.15rem] text-base">mestských<br />častí</span>
+                </div>
+                <div class="flex items-start gap-x-2">
+                    <span class="text-4xl font-medium">{{ $stats['lastUpdate']->isoFormat('DD/MM/YY') }}</span>
+                    <span class="leading-none mt-[0.15rem] text-base">naposledy<br />aktualizované</span>
+                </div>
             </div>
         </div>
 
