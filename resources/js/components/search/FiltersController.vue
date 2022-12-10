e565b5beb80e1f091aa5e9f8ceabc6068581ff65
Ernest Walzel <ewalzel@gmail.com>
2022-12-10 09:15:42 +0100

    replaceState instead of pushState

--- resources/js/components/search/FiltersController.vue
+++ resources/js/components/search/FiltersController.vue
@@ -68,7 +68,7 @@ export default {
                 }
             );
 
-            window.history.pushState(
+            window.history.replaceState(
                 newUrl,
                 '', // unused param
                 newUrl
