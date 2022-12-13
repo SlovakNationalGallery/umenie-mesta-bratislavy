eb73bc20969fec10e8026cb2234231c06b402f95
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2022-12-13 23:04:07 +0100

    feat: add all filters

--- resources/views/artworks/index.blade.php
+++ resources/views/artworks/index.blade.php
@@ -3,11 +3,11 @@
 @section('content')
     <section class="max-w-screen-3xl px-4 md:px-14 mx-auto">
         <div class="my-4">
-            <search.filters-controller v-cloak v-slot="{ filters, query, onCheckboxChange, onOpenedFilterChange, openedFilterName }">
+            <search.filters-controller v-cloak
+                v-slot="{ filters, query, onCheckboxChange, onOpenedFilterChange, openedFilterName }">
                 <div class="grid grid-cols-4">
                     <search.multi-select id="filter-borough-" label="borough" name="boroughs"
-                        :is-filter-opened="openedFilterName === 'boroughs'"
-                        :on-checkbox-change="onCheckboxChange"
+                        :is-filter-opened="openedFilterName === 'boroughs'" :on-checkbox-change="onCheckboxChange"
                         :on-label-click="onOpenedFilterChange"
                         :options="filters.boroughs?.map(option =>
                             ({
@@ -15,9 +15,42 @@
                                 value: option.value,
                                 checked: query.boroughs.includes(option.value)
                             })
-                        )"
-                        >
+                        )">
                     </search.multi-select>
+                    <search.multi-select id="filter-author-" label="author" name="authors"
+                        :is-filter-opened="openedFilterName === 'authors'" :on-checkbox-change="onCheckboxChange"
+                        :on-label-click="onOpenedFilterChange"
+                        :options="filters.authors?.map(option =>
+                            ({
+                                label: `${option.label} ${option.count}`,
+                                value: option.value,
+                                checked: query.authors.includes(option.value)
+                            })
+                        )">
+                    </search.multi-select>
+                    <search.multi-select id="filter-category-" label="category" name="categories"
+                        :is-filter-opened="openedFilterName === 'categories'" :on-checkbox-change="onCheckboxChange"
+                        :on-label-click="onOpenedFilterChange"
+                        :options="filters.categories?.map(option =>
+                            ({
+                                label: `${option.label} ${option.count}`,
+                                value: option.value,
+                                checked: query.categories.includes(option.value)
+                            })
+                        )">
+                    </search.multi-select>
+                    <search.multi-select id="filter-keyword-" label="keyword" name="keywords"
+                        :is-filter-opened="openedFilterName === 'keywords'" :on-checkbox-change="onCheckboxChange"
+                        :on-label-click="onOpenedFilterChange"
+                        :options="filters.keywords?.map(option =>
+                            ({
+                                label: `${option.label} ${option.count}`,
+                                value: option.value,
+                                checked: query.keywords.includes(option.value)
+                            })
+                        )">
+                    </search.multi-select>
+
                 </div>
             </search.filters-controller>
         </div>
