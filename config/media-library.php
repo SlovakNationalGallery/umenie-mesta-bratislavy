3f30e3e78e45b36873e8b345a4904c6df9afee40
Ernest Walzel <ewalzel@gmail.com>
2022-12-15 16:49:34 +0100

    Allow 20MB images GMBUVP-30

--- config/media-library.php
+++ config/media-library.php
@@ -8,6 +8,12 @@ return [
     'prefix' => env('MEDIA_PREFIX', '/media'),
 
     /*
+     * The maximum file size of an item in bytes.
+     * Adding a larger file will result in an exception.
+     */
+    'max_file_size' => 1024 * 1024 * 20,
+
+    /*
      * The fully qualified class name of the media model.
      */
     'media_model' => App\Models\Media::class,
