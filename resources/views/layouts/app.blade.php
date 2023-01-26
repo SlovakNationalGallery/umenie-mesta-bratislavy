b4c69951f9a61ecb10f751e8dc829d7bccdb97f7
Ernest Walzel <eronisko@users.noreply.github.com>
2023-01-26 07:25:41 +0000

    Fix clarity quotes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,12 +17,12 @@
 
     @production
         {{-- prettier-ignore-start --}}
-        <script type=“text/javascript”>
+        <script type="text/javascript">
             (function(c,l,a,r,i,t,y){
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src=“https://www.clarity.ms/tag/”+i;
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, “clarity”, “script”, “enuuzaniox”);
+            })(window, document, "clarity", "script", "enuuzaniox");
         </script>
 
         {{-- GA4 --}}
@@ -164,4 +164,3 @@
 </body>
 
 </html>
-
