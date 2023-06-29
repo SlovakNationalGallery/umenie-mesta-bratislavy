eb24d6113e2693a67eca60ffb7173740712b4a23
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2023-06-29 18:39:13 +0200

    fix: migration

--- database/migrations/2023_06_29_151653_add_photo_source.php
+++ database/migrations/2023_06_29_151653_add_photo_source.php
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('photos', function (Blueprint $table) {
-            $table->dropColumn('source')->nullable();
+            $table->dropColumn('source');
         });        
     }
 };
