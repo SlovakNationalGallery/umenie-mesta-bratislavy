d68f91741247804473a43a2b77e03f1095994138
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2024-07-09 16:17:23 +0200

    Fix shrinking icon

--- resources/views/artworks/show.blade.php
+++ resources/views/artworks/show.blade.php
@@ -128,7 +128,7 @@
                     @if ($artwork->condition_note)
                         <div class="flex text-neutral-500">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="mr-1 mt-[0.15rem] h-5 w-5">
+                                stroke="currentColor" class="mr-1 mt-[0.15rem] h-5 w-5 flex-none">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
                             </svg>
