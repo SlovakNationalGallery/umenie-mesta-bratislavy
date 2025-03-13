d39ee31c8a5b01d301a005acb1be39ba011bdf60
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2025-03-13 10:15:45 +0100

    feat: add material & state filter (#74)

---
+++ resources/js/components/search/TransitionOpacity.vue
@@ -0,0 +1,10 @@
+<template>
+    <transition
+    enter-from-class="opacity-0"
+    leave-to-class="opacity-0"
+    enter-active-class="transition-all ease-out duration-100"
+    leave-active-class="transition-all ease-out duration-100"
+    >
+        <slot />
+    </transition>
+</template>
